8ff407de474a43f00d4c1e29fdab731fb742eedd
ymwoo <ymwoo@ubivelox.com>
2021-05-05 17:47:31 +0900

    base최초등록

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        solution ("" );
+    }
+
+    public static String solution(String param) {
+        return "";
+    }
+}
