c303a886bc330affc5eb327dbd68bed662a0961d
ymwoo <ymwoo@ubivelox.com>
2021-05-05 17:53:18 +0900

    해시 > 완주하지 못한 선수
    테스트 완료 커밋 진행

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,39 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        solution ("" );
+        String[] participant = new String[]{"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = new String[]{"josipa", "filipa", "marina", "nikola"};
+        solution (participant, completion);
     }
 
-    public static String solution(String param) {
-        return "";
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap <String, Integer> map =new HashMap<>();
+        for(String key : participant){
+            if(map.containsKey(key)){
+                map.put(key, map.get(key)+1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        for(String key : completion){
+            map.put(key, map.get(key)-1);
+        }
+
+        for(String key : participant){
+            if(map.get(key)!=0){
+                answer=key;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+        return answer;
     }
 }
